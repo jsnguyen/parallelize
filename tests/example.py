5c89803248eb4e05b921a3bc98cb40acc8e69c50
Jayke Nguyen <jsn@saved.local>
2024-08-02 17:14:48 -0700

    updated example

--- tests/example.py
+++ tests/example.py
@@ -3,21 +3,33 @@ from parallelize import parallelize
 
 def main():
     num = 10**7  # Large number to make the task computationally heavy
-    data = [num] * 128  # Repeat the task 8 times for parallel processing
+    data = [num] * 64 # Repeat the task 8 times for parallel processing
 
     start_time = time.time()
-    @parallelize(data, pool_size=16, use_tqdm=True, enum=True)
+
+    @parallelize(data, pool_size=1, use_tqdm=True, enum=True)
     def compute_heavy_task(data):
         num, ii  = data
-        print(ii)
         result = sum(i * i for i in range(num))
         return result
 
     results = compute_heavy_task()
     end_time = time.time()
 
-    print(f"Results: {results}")
-    print(f"Time taken: {end_time - start_time:.3f} seconds")
+    print(f"Single process time taken: {end_time - start_time:.3f} seconds")
+
+    start_time = time.time()
+
+    @parallelize(data, use_tqdm=True, enum=True)
+    def compute_heavy_task(data):
+        num, ii  = data
+        result = sum(i * i for i in range(num))
+        return result
+
+    results = compute_heavy_task()
+    end_time = time.time()
+
+    print(f"Multi process time taken: {end_time - start_time:.3f} seconds")
 
 if __name__ == "__main__":
     main()
